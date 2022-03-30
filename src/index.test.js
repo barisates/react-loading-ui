ec1709c1b37b67852c3ea12a62f04d7bf49d32ca
Samet Yazıcı <sametyzc@hotmail.com>
2022-03-30 11:24:34 +0300

    functions added to the test file

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,13 @@
-import {Loading} from './index';
+import {Loading, HideLoading, ShowLoading} from './index';
 
 it('renders without crashing', () => {
     Loading();
     Loading({progress: true});
+});
+
+it('renders without crashing', () => {
+    HideLoading();
+    ShowLoading({progress: true});
+    ShowLoading();
+    HideLoading();
 });
