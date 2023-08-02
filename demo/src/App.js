12c895c17ad2e105c46c8e3baefc9da263683599
Özlem <48884511+ozlemkcr@users.noreply.github.com>
2023-08-02 16:56:55 +0300

    Upgraded to react 18
    - Upgraded to react 18 version.
    - Converted class component to functional component.

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,74 +1,55 @@
-import React, { Component } from 'react';
+import React, { useRef, useState } from 'react';
 import { Loading, Progress, SetDefault } from 'react-loading-ui'
 import './App.css';
 
 let interval = null;
 
-class App extends Component {
-  constructor() {
-    super()
+const App =() =>{
+  const progress = useRef(1);
+  const [theme, setTheme] = useState("dark");
 
-    this.state = {
-      progress: 1,
-      theme: "dark"
-    }
-
-  }
-  DefaultLoading() {
+  const DefaultLoading = () => {
     Loading();
     setTimeout(() => { Loading() }, 3000);
   }
-  TopbarLoading() {
+  const TopbarLoading = () =>  {
     Loading({ topBar: true });
     setTimeout(() => { Loading() }, 3000);
   }
-  LoadingProgress() {
-
+  const LoadingProgress = () =>  {
     Loading({
       progress: true,
       progressedClose: true
     });
 
     interval = setInterval(() => {
-
-      this.setState(
-        { progress: this.state.progress + 4 },
-        () => {
-
-          // Set Progress Value
-          Progress(this.state.progress);
-
-          if (this.state.progress >= 100) {
-            this.setState({ progress: 0 });
-            clearInterval(interval);
-          }
-
-
-        });
-
+      progress.current = progress.current + 4;
+      Progress(progress.current);
+      if (progress.current >= 100) {
+        progress.current = 0;
+        clearInterval(interval);
+      }
     }, 100);
 
   }
-  render() {
     return (
       <div className="App">
         <header className="App-header">
           <h5 className="mb-3">React Loaing Component</h5>
           <div>
-            <button type="button" className="btn btn-sm btn-dark mr-4" onClick={(e) => this.DefaultLoading()}>Show Loading</button>
-            <button type="button" className="btn btn-sm btn-dark mr-4" onClick={(e) => this.TopbarLoading()}>Show Top Bar Loading</button>
-            <button type="button" className="btn btn-sm btn-dark" onClick={(e) => this.LoadingProgress()}>Show Progress</button> <br />
-            <button type="button" className="btn btn-sm btn-light" onClick={(e) => {
-              SetDefault({ theme: this.state.theme });
-              this.setState({ theme: (this.state.theme === "light" ? "dark" : "light") })
-            }}>Set Default Theme: {this.state.theme}</button>
+            <button type="button" className="btn btn-sm btn-dark mr-4" onClick={DefaultLoading}>Show Loading</button>
+            <button type="button" className="btn btn-sm btn-dark mr-4" onClick={TopbarLoading}>Show Top Bar Loading</button>
+            <button type="button" className="btn btn-sm btn-dark" onClick={LoadingProgress}>Show Progress</button> <br />
+            <button type="button" className="btn btn-sm btn-light" onClick={() => {
+              SetDefault({ theme: theme });
+              setTheme(theme === "light" ? "dark" : "light")
+            }}>Set Default Theme: {theme}</button>
           </div>
           <div className="author">
             <a href="https://github.com/barisates" target="_blank" rel="noopener noreferrer">barisates</a>
           </div>
         </header>
       </div>)
-  }
 }
 
 
